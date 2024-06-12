e89377145039e09b375fa077cd35e572f6e9f1ed
TitusKI <kidumazie@gmail.com>
2024-06-12 16:39:59 +0300

    Data Migration

--- crudapi/api/views.py
+++ crudapi/api/views.py
@@ -3,6 +3,8 @@ from rest_framework import generics
 from .models import BlogPost
 from .serializer import BlogPostSerializer
 # Create your views here.
+
+# BlogPostListCreate : handles GET(list) and POST(create)
 class BlogPostListCreate(generics.ListCreateAPIView):
     queryset = BlogPost.objects.all()
     serializer_class = BlogPostSerializer
