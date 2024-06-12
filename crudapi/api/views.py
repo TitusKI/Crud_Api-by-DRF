f7e3ab9332f9bcf83fe130d54734b311a7333752
TitusKI <kidumazie@gmail.com>
2024-06-12 22:50:27 +0300

    DeleteView

--- crudapi/api/views.py
+++ crudapi/api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, status 
+from rest_framework.response import Response
 from .models import BlogPost
 from .serializer import BlogPostSerializer
 # Create your views here.
@@ -8,3 +9,12 @@ from .serializer import BlogPostSerializer
 class BlogPostListCreate(generics.ListCreateAPIView):
     queryset = BlogPost.objects.all()
     serializer_class = BlogPostSerializer
+    
+    def delete(self, request, *args, **kwargs):
+        BlogPost.objects.all().delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class BlogPostRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerializer
+    lookup_field = 'pk'
